a61091c9599ed070e68980545536b23dfe559e75
nerodnoy <thatsforsure@mail.ru>
2024-05-05 15:25:23 +0300

    added content for templates

--- spb_guide/restaurants/views.py
+++ spb_guide/restaurants/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from .models import Restaurants
 
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
 
 def index(request):
-    return render(request, 'restaurants/index.html', {'menu': menu, 'title': 'Главная страница'})
+    posts = Restaurants.objects.all()
+    return render(request, 'restaurants/index.html', {'posts': posts,'menu': menu, 'title': 'Главная страница'})
 
 
 def about(request):
