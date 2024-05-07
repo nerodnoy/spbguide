136e278d92c39f10eb2d7bb861b41c9f942a37bb
nerodnoy <thatsforsure@mail.ru>
2024-05-07 15:22:41 +0300

    reworked names

--- spb_guide/restaurants/views.py
+++ spb_guide/restaurants/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from .models import Restaurants
 
 menu = [{'title': 'О сайте', 'url_name': 'about'},
-        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Добавить заведение', 'url_name': 'add_page'},
         {'title': 'Обратная связь', 'url_name': 'contact'},
         {'title': 'Войти', 'url_name': 'login'},
         ]
