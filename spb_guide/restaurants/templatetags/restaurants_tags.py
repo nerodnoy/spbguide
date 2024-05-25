6959654a40273e50a550c57d5096e36d1b9487da
nerodnoy <thatsforsure@mail.ru>
2024-05-25 13:08:53 +0300

    modified tags and added it to the template

--- spb_guide/restaurants/templatetags/restaurants_tags.py
+++ spb_guide/restaurants/templatetags/restaurants_tags.py
@@ -5,11 +5,17 @@ register = template.Library()
 
 
 @register.simple_tag(name='get_cats')
-def get_categories():
-    return Category.objects.all()
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
 
 
 @register.inclusion_tag('restaurants/list_categories.html')
-def show_categories():
-    cats = Category.objects.all()
-    return {'cats': cats}
+def show_categories(sort=None, cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+    return {'cats': cats, 'cat_selected': cat_selected}
