ade5059b45790b19fa181f7adedf0fdf5449d10d
nerodnoy <thatsforsure@mail.ru>
2024-05-25 12:25:58 +0300

    added an admin panel with multiple sorting options

--- spb_guide/restaurants/admin.py
+++ spb_guide/restaurants/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Restaurants)
+class RestaurantsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
--- spb_guide/restaurants/models.py
+++ spb_guide/restaurants/models.py
@@ -3,16 +3,16 @@ from django.urls import reverse
 
 
 class Restaurants(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Содержание')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Посл. изменения')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
 
     # _id будет добавлено в имя автоматически Джангой
     # null=True временно заполнит новые поля нулями. Это нужно при makemigrations
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -21,6 +21,11 @@ class Restaurants(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Популярные рестораны'
+        verbose_name_plural = 'Популярные рестораны'
+        ordering = ['title']
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
@@ -28,5 +33,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    # Благодаря этой функции админ-панель демонстрирует ссылку на ресторан
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
