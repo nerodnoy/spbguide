00031bfaa36cd4c09fdfa60abb1032b07964418e
nerodnoy <thatsforsure@mail.ru>
2024-05-28 13:27:35 +0300

    added forms (linked to models) to add new pages /// added custom Validations

--- spb_guide/restaurants/forms.py
+++ spb_guide/restaurants/forms.py
@@ -1,10 +1,45 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import *
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length=255, label='URL', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Контент')
-    is_published = forms.BooleanField(label='Публикация', required=False, initial=True)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Категория не выбрана')
+# Если планирую использовать формы, которые не связаны с моделью
+# class AddPostForm(forms.Form):
+#     title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
+#     slug = forms.SlugField(max_length=255, label='URL', widget=forms.TextInput(attrs={'class': 'form-input'}))
+#     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Контент')
+#     is_published = forms.BooleanField(label='Публикация', required=False, initial=True)
+#     cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Нет категории')
+
+
+class AddPostForm(forms.ModelForm):
+    # Если я хочу, чтобы изначально было что-то изменено в форме
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = 'Категория не выбрана'
+
+    class Meta:
+        model = Restaurants
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 40, 'rows': 10}),
+        }
+
+    # Если я хочу сделать пользовательскую проверку
+    # Проверяю поле title
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов')
+
+        return title
+
+    # Проверяю поле content
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if 'гадость' in content:
+            raise ValidationError('Вы написали плохое слово')
+
+        return content
