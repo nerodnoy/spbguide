4de4b0d16133c4e3d6b361952296c0f21669255b
nerodnoy <thatsforsure@mail.ru>
2024-06-25 14:04:42 +0300

    added registration for users

--- spb_guide/restaurants/urls.py
+++ spb_guide/restaurants/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('add_page/', AddPage.as_view(), name='add_page'),
-    path('login/', login, name='login'),
+    path('login/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>/', RestaurantsCategory.as_view(), name='category')
 ]
--- spb_guide/restaurants/views.py
+++ spb_guide/restaurants/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 
 from .forms import *
 from .models import *
@@ -134,3 +136,14 @@ class RestaurantsCategory(DataMixin, ListView):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = UserCreationForm
+    template_name = 'restaurants/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
